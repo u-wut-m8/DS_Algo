f246165bfe1383f1d24f16c64a65387a1b4aad10
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2019-01-31 22:59:56 +0530

    Sort.

--- xyz.cpp
+++ xyz.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
+#include <algorithm>
+#include <climits>
+#include <vector>
 
-int solve(std::string p, std::string s) {
-	if (s.size() % p.size() != 0)
-		return 0;
-	if (p == s)
-		return 1;
-	while(p.size() <= s.size()){
-		p += p;
-		if(p == s)
-			return 1;
-	}
-	return 0;
-}
-
-int main(int argc, char const *argv[])
-{
-	std::string p, s;
-	int T;
+int main() {
+	//code
+	int T, N, M;
+	long long *arr, min;
 	std::cin>>T;
 	while(T--) {
-		std::cin>>p>>s;
-		std::cout<<solve(p, s)<<std::endl;
-		s.clear();
-		p.clear();
+	    std::cin>>N;
+	    min = INT_MAX;
+	    arr = new long long[N];
+	    for(int i=0;i<N;i++)
+	        std::cin>>*(arr + i);
+	    std::cin>>M;
+	    std::sort(arr, arr+N);
+		std::vector<long long> first(arr, arr+M), last(arr+M, arr+N);
+	    for(int i=0;i<first.size();i++)
+			std::cout<<first[i]<<" ";
+		std::cout<<std::endl;
+		for(int i=0;i<last.size();i++)
+			std::cout<<last[i]<<" ";
+		std::vector<long long> v(first.size()*last.size());
+		for(int i=0;i<first.size();i++)
+		
+		std::cout<<std::endl;
+	    delete[] arr;
 	}
-    return 0;
+	return 0;
 }
