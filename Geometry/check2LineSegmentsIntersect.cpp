fa485952d34ff7cddb90118a23ccb514e81d4c79
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2019-02-09 00:00:14 +0530

    Geometry update.

--- Geometry/check2LineSegmentsIntersect.cpp
+++ Geometry/check2LineSegmentsIntersect.cpp
@@ -16,7 +16,7 @@ bool onSegment(Point p, Point q, Point r) {
     return false;
 }
 
-bool doIntersect(Point p1, Point p2, Point q1, Point q2) {
+bool doIntersect(Point p1, Point q1, Point p2, Point q2) {
     int o1 = orientation(p1, q1, p2);
     int o2 = orientation(p1, q1, q2);
     int o3 = orientation(p2, q2, p1);
