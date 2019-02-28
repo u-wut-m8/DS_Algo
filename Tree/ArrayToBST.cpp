fef78b92d947009a31b9f4a6b36d4f3c591795c5
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2019-02-28 16:20:17 +0530

    Array To BST.

---
+++ Tree/ArrayToBST.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+struct node {
+    int data;
+    node *left, *right;
+};
+
+inline node* newnode(const int& val){
+    return new node{val, nullptr, nullptr};
+}
+
+//Unbalanced BST to Balance BST
+void inorder(std::vector<node*>& v, node* root){
+    if(root){
+        inorder(v, root->left);
+        v.push_back(root);
+        inorder(v, root->right);
+    }
+}
+
+node* balanceUtil(std::vector<node*>& v, int start, int end){
+    if(start > end)
+        return nullptr;
+    int mid = (start+end)>>1;
+    node* temp = v[mid];
+    temp->left = balanceUtil(v, start, mid-1);
+    temp->right = balanceUtil(v, mid+1, end);
+    return temp;
+}
+
+node* balancedTree(node* root){
+    std::vector<node*> nodes;
+    inorder(nodes, root);
+    return balanceUtil(nodes, 0, nodes.size()-1);
+}
+
+//Create BST from array
+node* insert(node* root, const int& val) {
+    if(!root)
+        return newnode(val);
+    else{
+        if(val <= root->data)
+            root->left = insert(root->left, val);
+        else
+            root->right = insert(root->right, val);
+    }
+}
+
+void preorder(node* root){
+    if(root){
+        std::cout<<root->data<<' ';
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int T, N, *arr;
+    node* head;
+    std::cin>>T;
+    while(T--){
+        std::cin>>N;
+        head = nullptr;
+        arr = new int[N];
+        for(int i=0;i<N;i++){
+            std::cin>>*(arr+i);
+            head = insert(head, *(arr+i));
+        }
+        head = balancedTree(head);
+        preorder(head);
+        std::cout<<std::endl;
+        delete[] arr;
+    }
+    return 0;
+}
