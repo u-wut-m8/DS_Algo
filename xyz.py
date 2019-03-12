1d45762d4063f82b4d847b9617634c952a263906
u-wut-m8 <sumanmit.padhi@outlook.com>
2019-03-13 01:42:11 +0530

    Heap sort.

--- xyz.py
+++ xyz.py
@@ -1,8 +1,4 @@
-T = int(input())
-while T:
-    N, d = input().split()
-    for _ in range(len(N)):
-        if N[:_] + N[_:+1] + d < N:
-            N = N[:_] + N[_:+1] + d
-    print(N)
-    T -= 1
+N, K = map(int, input().split())
+arr = [*map(int, input().split())]
+temp = [0]*(10**9)
+print("lmao")
